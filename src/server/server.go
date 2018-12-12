d0fb58b89626b4c72d46e4520e57fd8d5782f6c6
Jeff Wang <s900274@gmail.com>
2018-12-12 10:11:03 +0800

    Add Room

--- src/server/server.go
+++ src/server/server.go
@@ -1,31 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/nkovacs/go-socket.io"
 )
+
+var room = make(map[string]int) //房间的详情
+
+type MsgStruct struct {
+	Username  string `json:"username"`
+	Message	  string `json:"msg"`
+	Uuid 	  string `json:"uuid"`
+	RoomId	  string `json:"roomId"`
+	Type	  string `json:"type"`
+}
 
 func main() {
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.On("connection", func(so socketio.Socket) {
+	_ = server.On("connection", func(so socketio.Socket) {
+
 		log.Println("on connection")
-		so.Join("chat")
-		//so.BroadcastTo("chat", "chat message", `{"username":"Test","msg":"111","uuid":"161Test"}`)
-		so.On("chat message", func(msg string) {
+
+		roomId := joinRoom(room)
+
+		room[roomId]++
+
+		_ = so.Join(roomId)
+
+		msgStruct := &MsgStruct{}
+		msgStruct.RoomId = roomId
+		msgStruct.Type	 = "connect"
+
+		respStr, _ := json.Marshal(msgStruct)
+		so.Emit("chat message", string(respStr))
+
+		log.Println("room list", room)
+		_ = so.On("chat message", func(msg string) {
+
+			log.Println(so.Id())
+			log.Println(so.Rooms())
+
+			msgStruct := &MsgStruct{}
+			json.Unmarshal([]byte(msg), msgStruct)
+
 			log.Println("chat message", msg)
+			log.Println(msgStruct)
 			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
+			switch msgStruct.Type {
+			case "message":
+				_ = so.BroadcastTo(msgStruct.RoomId, "chat message", msg)
+			case "leave":
+				log.Println("room list", room)
+				//room[msgStruct.RoomId]--
+				if room[msgStruct.RoomId] == 1 {
+					delete(room, msgStruct.RoomId)
+				} else {
+					room[msgStruct.RoomId]--
+				}
+				so.Leave(msgStruct.RoomId)
+			}
 		})
-		so.On("disconnection", func() {
+		_ = so.On("disconnection", func() {
 			log.Println("on disconnect")
 		})
 	})
-	server.On("error", func(so socketio.Socket, err error) {
+	_ = server.On("error", func(so socketio.Socket, err error) {
 		log.Println("error:", err)
 	})
 
@@ -34,3 +81,15 @@ func main() {
 	log.Println("Serving at localhost:12345...")
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
+
+func joinRoom(roomList map[string]int) (string){
+
+	for k, v := range roomList {
+
+		if v == 1 {
+			return k
+		}
+	}
+
+	return "Room_"+strconv.FormatInt(time.Now().UnixNano(), 10)
+	}
